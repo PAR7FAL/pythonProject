c5829321dd70cd2183e2e2afa74a88c35d92ac4d
PAR7FAL <a.matvey.r@gmail.com>
2024-07-24 01:36:27 +0300

    Initial Commit

--- routers/book.py
+++ routers/book.py
@@ -4,6 +4,8 @@ from db import get_session
 from models import Book, User
 from utils import verify_access_token
 from schemas import Newbook
+from schemas import GetUser
+from schemas import Location
 router = APIRouter(tags=['book'],
                    responses={404: {"description": "Not found"}})
 
@@ -21,3 +23,12 @@ def new_book(data: Newbook, user: User = Depends(verify_access_token), session: 
 def get_book(id: int, session: Session = Depends(get_session)):
     return session.exec(select(Book).where(Book.id == id)).first()
 
+@router.get('/location')
+def sm_location(data: Location, user: User = Depends(verify_access_token), session: Session = Depends(get_session)):
+    if not user:
+        raise HTTPException(status_code=400, detail='Хрень сделал')
+
+    lok = Location(location=data.location)
+    session.add(lok)
+    session.commit()
+    return 'compleate'
